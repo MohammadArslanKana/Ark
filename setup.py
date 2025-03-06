9d74bff0df19b101c912dd915648279f3ed63883
Mady520 <samadmudi5@gmail.com>
2025-03-06 06:19:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
+import shutil
 from setuptools import setup, find_packages
+
+def post_install():
+    bin_path = "/usr/local/bin/ark"
+    script_path = os.path.expanduser("~/.local/bin/ark")
+    if os.path.exists(script_path):
+        shutil.copy(script_path, bin_path)
+        os.chmod(bin_path, 0o755)  # Make it executable
+
 
 setup(
     name="ark",
